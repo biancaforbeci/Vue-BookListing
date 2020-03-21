91dcc9cec0d6c5c0580f66a12c853b4da65a992f
Bianca <biaformic@hotmail.com>
2020-03-21 18:18:48 -0300

    Adicionando v-for

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="book in books"> {{book.title}} : {{book.author}}</li>    
+    </ul>
   </div>
 </template>
 
@@ -10,6 +13,11 @@ export default {
   data() {
     return {
       title: 'All Books',
+      books: [
+                  {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+                  {title: 'American Gods', author: 'Neil Gaiman'},
+                  {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+             ],
     };
   },
 };
